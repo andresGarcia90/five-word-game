e877ab2f879c7a527226f03cce6d4e0d22f86784
andresGarcia90 <garcia.andi90@gmail.com>
2024-02-25 11:06:00 -0300

    Adding Navbar

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { HiLightBulb } from "react-icons/hi";
+import { HiOutlineMoon } from "react-icons/hi";
+import { HiOutlineInformationCircle } from "react-icons/hi";
+
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    if (!isDarkTheme) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    setIsDarkTheme(!isDarkTheme);
+  }
+  return (
+    <div className="bg-white dark:bg-slate-800 block">
+      <div className="flex justify-between px-5 py-2">
+        <HiOutlineInformationCircle className="dark:text-white text-xl" />
+        <h1 className="dark:text-white text-xl">
+          Five Letter puzzle
+        </h1>
+        <button className="" 
+          onClick={toggleTheme}>
+          {isDarkTheme && <HiOutlineMoon className="dark:text-white text-xl"/>}
+          {!isDarkTheme && <HiLightBulb className="text-xl"/>}
+        </button>
+      </div>
+    </div>
+  )
+}
